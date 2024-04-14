f1fd9c077a172b5d63f869eba1328bb2a86281b9
James Campbell <59-james@git.enigmalabs.io>
2024-04-14 00:08:29 -0400

    first

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Sometimes Less Wrong by James Campbell';
+export const SITE_DESCRIPTION = 'James Campbell\'s thoughts now on the web.';
