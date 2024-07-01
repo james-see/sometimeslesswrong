188c0a2ba60dfb06014c9c29bc1b8dc7ad33bbf0
James Campbell <59-james@git.enigmalabs.io>
2024-06-30 20:26:48 -0400

    added a new article and added linkable sections for numbered lists

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Build the project
+npm run build
+
+# Deploy to the remote server
 scp -r dist/ do:sometimeslesswrong/site/
+
+# Print a success message
+echo "Deployment successful!"
